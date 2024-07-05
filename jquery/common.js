af986475300278c07df6b06bc366f22edfd70d87
GFZ\shw5691 <dustmzmfla@naver.com>
2024-07-05 13:29:11 +0900

    추가 / 삭제 기능

--- jquery/common.js
+++ jquery/common.js
@@ -1,11 +1,24 @@
-
 function save(e){
     const text = $(e.target).parent().siblings().val();
+
+    const textContent = `
+        <div class="content">
+            <textarea placeholder="메모">${text}</textarea>
+            
+            <div class="btns">
+                <button class="delete" onclick="contentDelete(event);">삭제</button>
+            </div>
+        </div>
+    `
 
     if ( text == null || text == undefined || text == '' ) {
         alert('fail');
         return false;
     }else {
-        console.log('asd');
+        $('.wrapper').append(textContent);
     }
+}
+
+function contentDelete(e){
+    $(e.target).parents('.content').remove();
 }
