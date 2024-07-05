edfe22a3a349d731b50d81d46d61714153eaa28a
GFZ\shw5691 <dustmzmfla@naver.com>
2024-07-05 13:20:53 +0900

    커밋

--- jquery/common.js
+++ jquery/common.js
@@ -1,4 +1,11 @@
 
-$(document).ready(function(){
+function save(e){
+    const text = $(e.target).parent().siblings().val();
 
-});
+    if ( text == null || text == undefined || text == '' ) {
+        alert('fail');
+        return false;
+    }else {
+        console.log('asd');
+    }
+}
