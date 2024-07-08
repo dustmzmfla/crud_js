a21a2d42b13e63527a2e51684156cc726bc848ef
GFZ\shw5691 <dustmzmfla@naver.com>
2024-07-08 14:43:07 +0900

    작업

--- jquery/common.js
+++ jquery/common.js
@@ -1,9 +1,19 @@
-function save(e){
-    const text = $(e.target).parent().siblings().val();
+console.log(localStorage);
 
-    const textContent = `
-        <div class="content">
-            <textarea placeholder="메모">${text}</textarea>
+// localStorage.clear();
+
+if ( localStorage.length > 0 ) {
+    for(let i=0; localStorage.length > i; i++){
+        const memo = localStorage.getItem('item' + (i + 1));
+        write(memo);
+    }
+}
+
+function write(e){
+    $('.content:first-child').after(
+    `
+        <div class="content" data-tab=item` + e + `>
+            <textarea placeholder="메모">` + e + `</textarea>
             
             <div class="btns">
                 <button class="edit" onclick="edit(event);">수정</button>
@@ -11,16 +21,30 @@ function save(e){
             </div>
         </div>
     `
+    );
+}
+
+function save(e){
+    const text = $(e.target).parent().siblings().val();
+    let keyLength = $('.content').length;
 
     if ( text == null || text == undefined || text == '' ) {
         alert('fail');
         return false;
     }else {
-        $(e.target).parent().siblings().val("");
-        $('.content:first-child').after(textContent);
+        reset(e);
+        localStorage.setItem("item" + keyLength , text);
+        write(text);
+        console.log(localStorage);
     }
 }
 
+function reset(e) {
+    $(e.target).parent().siblings().val('');
+}
+
 function contentDelete(e){
+    const dataKey = $(e.target).parents('.content').attr('data-tab');
+    localStorage.removeItem(dataKey);
     $(e.target).parents('.content').remove();
 }
