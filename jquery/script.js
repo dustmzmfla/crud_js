f6fd9de79865f6a88d2396aa0c1cc00a710cad2c
GFZ\shw5691 <dustmzmfla@naver.com>
2024-07-10 13:47:05 +0900

    기능 구현 끝

--- jquery/script.js
+++ jquery/script.js
@@ -1,3 +1,5 @@
+const memoArr = [];
+
 function resetStorage(e){
     localStorage.clear();
     location.reload();
@@ -8,11 +10,31 @@ if (localStorage.length > 0) {
         let getData = JSON.stringify(localStorage.key((i)));
         let getKey = getData.slice(1, length-1);
         let getText = JSON.stringify(localStorage.getItem(getKey)).slice(1, length-1);
-        
+
         const sendData = { key: getKey, text: getText };
-        write(sendData)
+        memoArr.push(sendData);
     }
 }
+
+function organize(){
+    memoArr.sort((a, b) => a.key - b.key);
+
+    memoArr.forEach((e)=>{
+        const sendData = { key: e.key, text: e.text };
+        write(sendData);
+    })
+}
+organize();
+
+// const testArr = [
+//     { num: 1, text: 1},
+//     { num: 3, text: 3},
+//     { num: 2, text: 2},
+//     { num: 5, text: 5}
+// ];
+// testArr.sort((a, b) => a.num - b.num );
+
+// console.log(testArr)
 
 function write(e){
     $('.content:first-child').after(
@@ -31,14 +53,29 @@ function write(e){
 
 function reset(e){ $(e.target).parent().siblings().val(""); }
 
+
+
 function save(e){
+    let newDate = new Date();
+    let year = String(newDate.getFullYear());
+    let month = String(newDate.getMonth() + 1);
+    let date = String(newDate.getDate());
+    let time = String(newDate.getHours());
+    let min = String(newDate.getMinutes());
+    let sec = String(newDate.getSeconds());
+
+    let totalDate = parseInt(year + month + date + time + min + sec);
+    
     let text = $(e.target).parent().siblings().val();
-    let key = text;
-    const sendData = { key: key, text: text }
+    let key = totalDate
+    const sendData = { key: totalDate, text: text }
+
+    memoArr.push(sendData);
     
     write(sendData);
     JSON.stringify(localStorage.setItem(key, text));
     reset(e);
+    location.reload();
 }
 
 function edit(e){
