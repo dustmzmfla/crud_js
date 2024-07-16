9c0a1baabd3a037a52411ab9555f6ddec1a3134f
GFZ\shw5691 <dustmzmfla@naver.com>
2024-07-16 13:07:56 +0900

    메모 수정 기능 추가

--- javascript/script.js
+++ javascript/script.js
@@ -74,16 +74,80 @@
 //     location.reload();
 // }
 
-//테스트
+// localStorage.clear();
+const memoArr = JSON.parse(localStorage.getItem('memo')) || [];
+
+let newDate = new Date();
+let year = newDate.getFullYear();
+let month = newDate.getMonth() + 1;
+let date = newDate.getDate();
+
+let format = year + "-" + (("00" + month.toString()).slice(-2)) + "-" + (("00" + date.toString()).slice(-2));
+
+if ( memoArr.length > 0 ) {
+    memoArr.forEach((i) => {
+        document.querySelector('.wrapper').insertAdjacentHTML('beforeend',
+            `<div class="content">
+                <textarea placeholder="메모" oninput="textChange(event);" onfocus="onFocus(event);">${i.memo}</textarea>
+                
+                <footer>
+                <div class="date">${i.format}</div>
+
+                <div class="btns">
+                    <button class="edit" onclick="editMemo(event);" disabled="disabled">수정</button>
+                    <button class="delete" onclick="remove();">삭제</button>
+                </div>
+                </footer>
+            </div>`
+        );
+    });
+}
+
+document.querySelector('.cancle').addEventListener('click', function(){
+    this.parentNode.parentNode.previousSibling.previousSibling.value = '';
+});
+
+function remove(){
+    
+}
+
+let previousMemo = '';
+let nextMemo = '';
+
+
+function textChange(e){
+    nextMemo = e.target.value;
+    
+    if ( previousMemo !== nextMemo ) {
+        e.target.nextElementSibling.children[1].children[0].disabled = false;
+    }else {
+        e.target.nextElementSibling.children[1].children[0].disabled = true;
+    }
+}
+
+function editMemo(e){
+    nextMemo = e.target.parentNode.parentNode.previousElementSibling.value;
+    if ( previousMemo !== nextMemo ) {
+        memo = nextMemo
+        for ( let i = 0; memoArr.length > i; i++ ) {
+            if ( memoArr[i].memo == previousMemo ) {
+                memoArr.splice(i, 1, {memo, format});
+            }
+        }
+        localStorage.clear();
+        localStorage.setItem('memo', JSON.stringify(memoArr));
+    }else {
+        return false;
+    }
+    e.target.disabled = true;
+}
+
+function onFocus(e){
+    previousMemo = e.target.value;
+}
 
 document.querySelector('.save').addEventListener('click',function(){
-    let memo = this.parentNode.previousSibling.previousSibling.value;
-    let newDate = new Date();
-    let year = newDate.getFullYear();
-    let month = newDate.getMonth() + 1;
-    let date = newDate.getDate();
-
-    let format = year + "-" + (("00" + month.toString()).slice(-2)) + "-" + (("00" + date.toString()).slice(-2));
+    let memo = this.parentNode.parentNode.previousSibling.previousSibling.value;
     
     if (memo == '' || memo == null || memo == undefined) {
         alert('빈칸');
@@ -91,17 +155,23 @@ document.querySelector('.save').addEventListener('click',function(){
     }else {
         document.querySelector('.wrapper').insertAdjacentHTML('beforeend',
             `<div class="content">
-                <textarea placeholder="메모">${memo}</textarea>
+                <textarea placeholder="메모" onfocus="onFocus(event);">${memo}</textarea>
                 
                 <footer>
                 <div class="date">${format}</div>
 
                 <div class="btns">
-                    <button class="edit" onclick="edit(event);">수정</button>
-                    <button class="delete" onclick="remove(event);">삭제</button>
+                    <button class="edit" disabled>수정</button>
+                    <button class="delete" onclick="remove();">삭제</button>
                 </div>
                 </footer>
             </div>`
         );
+
+        this.parentNode.parentNode.previousSibling.previousSibling.value = '';
+
+        
+        memoArr.push({memo, format});
+        localStorage.setItem('memo', JSON.stringify(memoArr));
     }
 });
